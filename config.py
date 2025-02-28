9322d025a082e75af5a36ec7f694ebb7ee75ba26
rzxbotz <rzxbots@gmail.com>
2025-02-28 09:39:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ BOT_TOKEN ="7951751315:AAGs875A1rmTex24RIVsL2jOxm3HpFM2F0o"
 DB_URI = "mongodb+srv://dudemusic111:dudemusic111@cluster0.df3yis2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 CAPTION = "<b>{file_name}</b>"
 SESSION = "AQHEYfYArza8tuQsT5co4kHb8YNzooMTU66-oWtKN-1f5pz2RVk_K5azmyUD-UKqG_zMyErocu_5uh5Y2vSNGBW6p9pTmF8Rmks2gUUeRT-K_KfsLxFTC4b_WMhlTnoXO0jRojeqv04CbEmVQKz0NN1mNNToane6gcUwKepQUv4LgBS9MTJ6QlKvx7kmCN6QadpxG2KYOJQCU0V5FFtuJuR9XmjAmvvRAP3HI_5mtV2tFGkIfWQIluASf1MbVgN9anhbZktCkBxTziWCJo5jkV-LdZOKZXkg0W-UCwnuy08Jg8OGjOYDeYV3OAJLAt3eMJNOp4rLDt9aeWtyWEr12eYRWjaJgQAAAAFYZ_nVAA"
+REGEX_PATTERN = ""
 
 class temp(object):
     CANCEL=False
