2dbcbf27050495dc0ba1147bc2cfd19a21ba8f01
rzxbotz <rzxbots@gmail.com>
2025-03-01 01:42:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 
 API_ID = int(17427408)
 API_HASH = "9699e632de895e7d566c241615a0e637"
-BOT_TOKEN ="7951751315:AAGs875A1rmTex24RIVsL2jOxm3HpFM2F0o"
+BOT_TOKEN ="5919513919:AAFJq8uqhYJRpu_vx-QWjyW0D9X3vSTwfms"
 DB_URI = "mongodb+srv://dudemusic111:dudemusic111@cluster0.df3yis2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-CAPTION = "<b>{file_name}</b>"
-SESSION = "AQHEYfYArza8tuQsT5co4kHb8YNzooMTU66-oWtKN-1f5pz2RVk_K5azmyUD-UKqG_zMyErocu_5uh5Y2vSNGBW6p9pTmF8Rmks2gUUeRT-K_KfsLxFTC4b_WMhlTnoXO0jRojeqv04CbEmVQKz0NN1mNNToane6gcUwKepQUv4LgBS9MTJ6QlKvx7kmCN6QadpxG2KYOJQCU0V5FFtuJuR9XmjAmvvRAP3HI_5mtV2tFGkIfWQIluASf1MbVgN9anhbZktCkBxTziWCJo5jkV-LdZOKZXkg0W-UCwnuy08Jg8OGjOYDeYV3OAJLAt3eMJNOp4rLDt9aeWtyWEr12eYRWjaJgQAAAAFYZ_nVAA"
-REGEX_PATTERN = ""
+REGEX_PATTERN = r"s\d{1,2}[\.\s]?e[p]?\d{1,2}|season|\bepisode\b"
+LOG_CHANNEL_ID = "-1001930341741"
 
 class temp(object):
     CANCEL=False
