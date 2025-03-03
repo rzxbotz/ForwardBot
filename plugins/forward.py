323e1fddec0e023b426ef25547bdb59a73dba34f
rzxbotz <rzxbots@gmail.com>
2025-03-04 02:31:25 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -99,7 +99,7 @@ async def forward(client, message):
                 f"⏭️ Skipped (No Episode Format): {skipped_msg}\n"
                 f"⏳ Elapsed Time: {elapsed_minutes} min"
                 )
-            await asyncio.sleep(1)
+            await asyncio.sleep(2)
         except Exception as a:
             print("Cant Edit Flood Wait")
             pass
