e9a942916a3dfdcd28d6589d01bfda10a1bc99ac
rzxbotz <rzxbots@gmail.com>
2025-03-04 20:09:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,12 @@ from pyrogram import Client
 
 API_ID = int(17427408)
 API_HASH = "9699e632de895e7d566c241615a0e637"
-BOT_TOKEN ="7951751315:AAGs875A1rmTex24RIVsL2jOxm3HpFM2F0o"
+BOT_TOKEN ="5919513919:AAFJq8uqhYJRpu_vx-QWjyW0D9X3vSTwfms"
 DB_URI = "mongodb+srv://dudemusic111:dudemusic111@cluster0.df3yis2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 CAPTION = "<b>{file_name}</b>"
-SESSION = "AQHEYfYArza8tuQsT5co4kHb8YNzooMTU66-oWtKN-1f5pz2RVk_K5azmyUD-UKqG_zMyErocu_5uh5Y2vSNGBW6p9pTmF8Rmks2gUUeRT-K_KfsLxFTC4b_WMhlTnoXO0jRojeqv04CbEmVQKz0NN1mNNToane6gcUwKepQUv4LgBS9MTJ6QlKvx7kmCN6QadpxG2KYOJQCU0V5FFtuJuR9XmjAmvvRAP3HI_5mtV2tFGkIfWQIluASf1MbVgN9anhbZktCkBxTziWCJo5jkV-LdZOKZXkg0W-UCwnuy08Jg8OGjOYDeYV3OAJLAt3eMJNOp4rLDt9aeWtyWEr12eYRWjaJgQAAAAFYZ_nVAA"
+SESSION = "AQEJ69AAK_rj6Zrdaja6JnvjpLQ7u_5iX0Qx9f6cB0RkB736_6n6wIm1qLNI-Dq77XkdjVcW2mjh_9ruiTmcsFB5r43bZn3EPUCHH928eGDP3q7exJ216dWn3rIj_Fp0glzPiiqowq9OvzzHa3AGVT4piyfy7rf7tOjvV1nnhBZyvVl6OFi-1hK5-UVowvzaTDf4W44WPzMG12w797FR0Auh4_EqbK2azcXkJu6wEzxY7_6AAfDiYM-lXh_7102XhfyrMNDj1v4aDOy9DwtYNwpxObH7LbrV_R5kyRdJ6_f9M94vymdNZsnfQibiQLLb2cD3gBN47CRKlY2phFVkikphguxckwAAAAFM6o2iAA"
+LOG_CHANNEL = "-1001930341741"
+DELETE_REGEX = r"s\d{1,2}[\.\s]?e[p]?\d{1,2}|season|\bepisode\b"
 
 class temp(object):
     CANCEL=False
