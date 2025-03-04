a2992360b6f1e9850908f17a824b3727458e72bb
rzxbotz <rzxbots@gmail.com>
2025-03-04 08:06:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, __version__, idle
 from pyrogram.raw.all import layer
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH, BOT_TOKEN, User
 from pyromod import listen
 from plugins import web_server
 from aiohttp import web
@@ -22,12 +22,15 @@ class Bot(Client):
   
     async def start(self):
         await super().start()
+        await User.start()
         me = await self.get_me()
+        usr = await User.get_me()
         srvr = web.AppRunner(await web_server())
         await srvr.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(srvr, "0.0.0.0", "8080").start()     
         print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        print(f"User is working on {usr.first_name}")
     
     async def stop(self, *args):
         await super().stop()
